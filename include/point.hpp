e0a5710b167e7047633b080a60457b143b9ad680
Vishal Kasliwal <vishal.kasliwal@gmail.com>
2023-06-05 21:18:16 -0700

    Add missing constructor

--- include/point.hpp
+++ include/point.hpp
@@ -79,6 +79,8 @@ class CelestialPoint {
         template <typename I>
         CelestialPoint(I hour, T min, T deg) : CelestialPoint(hour, 0, static_cast<T>(60.0)*min, 0, 0, static_cast<T>(3600.0)*deg) {};
 
+        CelestialPoint(T hour, T deg) : CelestialPoint(0, 0, static_cast<T>(3600.0)*hour, 0, 0, static_cast<T>(3600.0)*deg) {};
+
         CelestialPoint(coordinates::RA<T> ra, coordinates::Dec<T> dec) {ra_val = ra; dec_val = dec;};
 
         T const RA() const {return ra_val.value();};
